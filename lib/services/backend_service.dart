677052a9164768ce435f166a655895f673a443e0
Àlex Ollé <alex.olle@estudiantat.upc.edu>
2022-10-10 18:55:19 +0200

    Tasca #138: Es guarda la selecció de l'usuari al backend, aixi com també es recupera. De manera general, s'ha fet una mini llibreria genèrica per a fer crides a la api.

---
+++ lib/services/backend_service.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+import 'package:flutter_config/flutter_config.dart';
+import 'package:http/http.dart' as http;
+
+class BackendService {
+  // Variable en el .env con la dirección del backend. Ejemplo: BACKEND_API_URL=http://192.168.56.1:8000/api/
+  static final String _baseUrl = FlutterConfig.get('BACKEND_API_URL');
+
+  /// Permite hacer un get genérico a cualquier endpoint de la api
+  /// @param endpoint: Endpoint al que se quiere hacer el get (ejemplo: users/language/?id=1)
+  /// @return: Devuelve un Future con el resultado del get, al cual se le debe hacer un then para obtener el resultado
+  static Future<http.Response> get(String endpoint) async {
+    http.Response response = await http.get(
+      Uri.parse(_baseUrl + endpoint),
+      headers: {"Accept": "application/json"},
+    );
+    return response;
+  }
+
+  /// Permite hacer un post genérico a cualquier endpoint de la api
+  /// @param endpoint: Endpoint al que se quiere hacer el post (ejemplo: users/language/)
+  /// @param jsonMap: Mapa con los datos que se quieren enviar en el post en formato clave valor. Ejemplo: {"language": "es"}
+  /// @return: Devuelve un Future con el resultado del post, al cual se le debe hacer un then para obtener el resultado
+  static Future<http.Response> post(
+      String endpoint, Map<String, dynamic> jsonMap) async {
+    http.Response response = await http.post(
+      Uri.parse(_baseUrl + endpoint),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: jsonEncode(jsonMap),
+    );
+    return response;
+  }
+
+  /// Permite hacer un put genérico a cualquier endpoint de la api
+  /// @param endpoint: Endpoint al que se quiere hacer el put (ejemplo: users/1/)
+  /// @param jsonMap: Mapa con los datos que se quieren enviar en el put en formato clave valor. Ejemplo: {"language": "es"}
+  /// @return: Devuelve un Future con el resultado del put, al cual se le debe hacer un then para obtener el resultado
+  static Future<http.Response> put(
+      String endpoint, Map<String, dynamic> jsonMap) async {
+    http.Response response = await http.put(
+      Uri.parse(_baseUrl + endpoint),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: jsonEncode(jsonMap),
+    );
+    return response;
+  }
+
+  /// Permite hacer un delete genérico a cualquier endpoint de la api
+  /// @param endpoint: Endpoint al que se quiere hacer el post (ejemplo: users/1/)
+  /// @return: Devuelve un Future con el resultado del delete, al cual se le debe hacer un then para obtener el resultado
+  static Future<http.Response> delete(String endpoint) async {
+    http.Response response = await http.delete(
+      Uri.parse(_baseUrl + endpoint),
+      headers: {"Accept": "application/json"},
+    );
+    return response;
+  }
+}
--- lib/services/generalServices/LanguagesService.dart
+++ lib/services/generalServices/LanguagesService.dart
@@ -2,12 +2,11 @@ import 'dart:convert';
 
 import 'package:devicelocale/devicelocale.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_config/flutter_config.dart';
-import 'package:http/http.dart' as http;
+import 'package:greenwheel/services/backend_service.dart';
 
 import '../../languages/lang_config.dart';
 
-final String baseUrl = FlutterConfig.get('BACKEND_API_URL') + "users/";
+const String baseUrl = "users/language/";
 
 class LocaleLanguage extends ChangeNotifier {
   static Locale _locale = LangConfig.getLocale('es');
@@ -15,9 +14,10 @@ class LocaleLanguage extends ChangeNotifier {
   Locale get locale => _locale;
 
   fetchLocale() async {
-    getUserLanguage().then((locale) async {
-      if (locale != null) {
-        changeLanguage(LangConfig.getLocale(locale));
+    BackendService.get(baseUrl).then((response) async {
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+        changeLanguage(LangConfig.getLocale(jsonResponse['language']));
       } else {
         String? device_locale = await Devicelocale.currentLocale;
         device_locale = device_locale!.substring(0, 2);
@@ -35,33 +35,13 @@ class LocaleLanguage extends ChangeNotifier {
     }
     _locale = locale;
     notifyListeners();
-    setUserLanguage(LangConfig.getStringFromLocale(locale));
+    var jsonMap = {'language': LangConfig.getStringFromLocale(locale)};
+    BackendService.put(baseUrl, jsonMap)
+        .then((value) => null)
+        .catchError((error) => print(error));
   }
-
   void clearLocale() {
     _locale = LangConfig.getLocale('es');
     notifyListeners();
   }
-}
-
-Future<String> getUserLanguage() async {
-  http.Response response = await http.get(
-    Uri.parse("${baseUrl}languages/"),
-    headers: {"Accept": "application/json"},
-  );
-  print("Response ${response.body.toString()}");
-  return response.body;
-}
-
-Future<String> setUserLanguage(String lang) async {
-  var jsonMap = {
-    "language": lang,
-  };
-  http.Response response = await http.put(
-    Uri.parse("${baseUrl}languages/"),
-    headers: {"Accept": "application/json"},
-    body: jsonEncode(jsonMap),
-  );
-  print("Response ${response.body.toString()}");
-  return response.body;
 }
