b7bbfddab661a5fd6b4d110a86319eeb638e1886
1251911 <andreu.orensanz@est.fib.upc.edu>
2022-10-10 08:54:45 +0200

    Tasca #96: S'ha posat el botó per geolocalitzar i està feta la funció però no acaba d'anar encara

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,17 +17,38 @@ class _HomePageState extends State<HomePage> {
 
   final LatLng _center = const LatLng(41.3874, 2.1686);
 
+  Set<Marker> markers = {};
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
+  /*
+  void _currentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    zLocationData currentLocation;
+    var location = new Location();
+    try {
+      currentLocation = await location.getLocation();
+    } on Exception {
+      currentLocation = null;
+    }
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        bearing: 0,
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 17.0,
+      ),
+    ));
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: const Text("Home Page"),
         actions: [
-          IconButton(icon: Icon(Icons.language), onPressed: _changeLanguage),
+          IconButton(icon: const Icon(Icons.language), onPressed: _changeLanguage),
         ],
       ),
       body: GoogleMap(
@@ -32,11 +57,62 @@ class _HomePageState extends State<HomePage> {
           target: _center,
           zoom: 11.0,
         ),
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
+        padding: EdgeInsets.only(bottom:MediaQuery.of(context).size.height*0.80),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Position position = await _determinePosition();
+          mapController.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 14.0,
+            ),
+          ));
+
+          markers.clear();
+
+          markers.add(Marker(markerId:  const MarkerId('currentLocation'), position: LatLng(position.latitude, position.longitude)));
+        }, /*_currentLocation,*/
+        backgroundColor: Colors.green,
+        child: IconButton(icon: const Icon(Icons.location_searching), onPressed: () {}),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
   void _changeLanguage() {
-    print("Change language");
+    if (kDebugMode) {
+      print("Change language");
+    }
   }
 }
