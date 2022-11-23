9a4e32ada68e6065acf46f54871f2d2d7ef65419
arnau.gimenez.bolta <arnau.gimenez.bolta@estudiant.upc.edu>
2022-11-23 01:21:24 +0100

    Tasca #219 routes fixed

--- lib/routes.dart
+++ lib/routes.dart
@@ -21,31 +21,33 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const HomePage(key: Key("HomePage")),
-    ),
-    GoRoute(
-      path: '/route/:lat/:long',
-      builder: (context, state) {
-        final long = state.params['long']!;
-        final lat = state.params['lat']!;
-        return RoutePage(
-          key: Key("RoutePage"),
-          lat: lat,
-          long: long,
-        );
-      },
-    ),
-    GoRoute(
-      path: '/booking',
-      builder: (context, state) => const MyBookings(key: Key("Booking")),
-    ),
-    GoRoute(
-      path: '/chargers/list',
-      builder: (context, state) =>
+      routes: [
+        GoRoute(
+          path: 'chargers/add',
+          builder: (context, state) => const AddCharger(key: Key("AddCharger")),
+        ),
+        GoRoute(
+          path: 'chargers/list',
+          builder: (context, state) =>
           const ChargeInfoList(key: Key("ChargersList")),
-    ),
-    GoRoute(
-      path: '/chargers/add',
-      builder: (context, state) => const AddCharger(key: Key("AddCharger")),
+        ),
+        GoRoute(
+          path: 'booking',
+          builder: (context, state) => const MyBookings(key: Key("Booking")),
+        ),
+        GoRoute(
+          path: 'route/:lat/:long',
+          builder: (context, state) {
+            final long = state.params['long']!;
+            final lat = state.params['lat']!;
+            return RoutePage(
+              key: Key("RoutePage"),
+              lat: lat,
+              long: long,
+            );
+          },
+        ),
+      ]
     ),
   ],
 );
