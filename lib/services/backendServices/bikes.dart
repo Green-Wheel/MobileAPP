8f221012f92778b867e1b9341d93de74a558199d
arnau.gimenez.bolta <arnau.gimenez.bolta@estudiant.upc.edu>
2022-11-23 22:43:42 +0100

    Tasca #46 Creacio de crides a la api per bikes

---
+++ lib/services/backendServices/bikes.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import '../backend_service.dart';
+
+class BikesService {
+  static Future<bool> addBike(Map<String, dynamic> data, images) async {
+    try {
+      var response = await BackendService.post('bikes/', data);
+      if (response.statusCode != 200) return false;
+      var json = jsonDecode(response.body);
+      var response2 = await BackendService.postFiles(
+          'publications/${json['id']}/upload/', images);
+      return response2.statusCode == 200;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  static Future<Map<String, dynamic>> getBikeInfo(int id) async {
+    Map<String, dynamic> result = {};
+    var response = await BackendService.get('bikes/$id/');
+    if (response.statusCode == 200) {
+      result = jsonDecode(response.body);
+    } else {
+      throw Exception('Error getting bike info');
+    }
+    return result;
+  }
+}
