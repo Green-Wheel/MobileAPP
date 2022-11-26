3df12a1562077d52b160880fd0946d94ad4efd36
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2022-11-26 19:31:35 +0100

    Tasca #75: intergarcion id bookings

--- lib/widgets/button_reserva_list.dart
+++ lib/widgets/button_reserva_list.dart
@@ -28,7 +28,7 @@ class _ButtonReservaListWidget extends State<ButtonReservaListWidget>{
             onPressed: () {
               //Redireccion a la pantalla del calendario
               GoRouter.of(context)
-                  .go('/bookings/resevation/');
+                  .go('/bookings/${widget.id}');
             },
             child: Row(
               children: const [
