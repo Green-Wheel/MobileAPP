9992957519a6e0fb9d97cc1e5a48d0827164fcc7
arnau.gimenez.bolta <arnau.gimenez.bolta@estudiant.upc.edu>
2022-12-28 17:31:07 +0100

    reportCharger fet

---
+++ lib/screens/chargers/widgets/report_type_selector.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReportTypeSelector extends StatefulWidget {
+  const ReportTypeSelector({Key? key}) : super(key: key);
+
+  @override
+  State<ReportTypeSelector> createState() => _ReportTypeSelectorState();
+}
+
+class _ReportTypeSelectorState extends State<ReportTypeSelector> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
