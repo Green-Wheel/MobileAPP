218d78b2bdb2a3a81650da6848b39265634a3bc2
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2022-12-29 12:35:52 +0100

    Tasca #338: card user feta, falta retocs amb el context de la pantalla + enroutament. Retoc al rating d'estrelles

--- lib/widgets/card_chat_users.dart
+++ lib/widgets/card_chat_users.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:greenwheel/widgets/input_text_message.dart';
+import 'package:greenwheel/widgets/rating_stars.dart';
+import 'package:greenwheel/widgets/stars_static_rate.dart';
 import 'package:greenwheel/widgets/username_rating_stars.dart';
 
 class CardChatUsersWidget extends StatefulWidget {
@@ -15,9 +17,15 @@ class CardChatUsersWidget extends StatefulWidget {
 }
 
 main(){
+  bool new_message = true;
   runApp(MaterialApp(
     home: Scaffold(
-      body: _cardUsers("username", 4.0, "last message", true),
+      body: Column(
+        children: [
+          SizedBox(height: 200),
+          _cardUsers("Miguel Gutiérrez", 3.5, "last message", new_message),
+        ],
+      )
     ),
   ),
   );
@@ -32,32 +40,91 @@ class _CardChatUsersWidget extends State<CardChatUsersWidget> {
 
 Widget _cardUsers(String username, double rate_user, String last_message_received, bool new_message) {
   //TODO: passar context
-  return ListTile(
-    shape:  const RoundedRectangleBorder(
-      side: BorderSide(
-        color: Colors.black,
-        width: 2,
+  return InkWell(
+    onTap: () {
+      //TODO: implementar ruta de chat
+      new_message = false;
+    },
+    child: SizedBox(
+      height: 80,
+      child: ListTile(
+        shape:  const RoundedRectangleBorder(
+          side: BorderSide(
+            color: Colors.green,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        leading: new_message ?  CircleAvatar(
+          radius: 25,
+          backgroundColor: Colors.white,
+          child: Column(
+            children: [
+             Padding(
+               padding: EdgeInsets.only(left: 35),
+               child:  CircleAvatar(
+                 radius: 6,
+                 backgroundColor: Colors.red,
+                 //opcional marcar numero de mensajes no vistos
+                 //child: Text("1", style: TextStyle(color: Colors.white, fontSize: 10),),
+               ),
+             ),
+              Icon(
+                size: 30,
+                  color: Colors.green,
+                  Icons.person
+              ),
+            ],
+          )
+        ): CircleAvatar(
+          radius: 25,
+          backgroundColor: Colors.white,
+          child: Icon(
+              size: 30,
+              color: Colors.green,
+              Icons.person
+          ),
+        ),
+        title: Column(
+          children: [
+            SizedBox(
+              height: 70,
+              child: Stack(
+                children: [
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: SizedBox(
+                        //TODO: retocar width pantalla
+                        width: 250,
+                        child: Column(
+                            children:[
+                              Padding(
+                                padding: EdgeInsets.only(right: 108),
+                                child: Text(username, style: const TextStyle(fontSize: 18, color: Colors.black),),
+
+                              ),
+                              //RatingStars(rating: rate_user.toString()),
+                              StarsStaticRateWidget(rate: rate_user),
+                              //SizedBox(height: 5),
+                              Padding(
+                                padding: EdgeInsets.only(right: 170),
+                                child: Text(last_message_received, style: TextStyle(fontSize: 10, color: Colors.grey)),
+                              ),
+                            ]
+                        ),
+                      )
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text("12:00", style: const TextStyle(fontSize: 15, color: Colors.black),),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      borderRadius: BorderRadius.all(Radius.circular(10)),
     ),
-    leading: new_message ?  CircleAvatar(
-      radius: 20,
-      backgroundColor: Colors.white,
-      child: Icon(
-          color: Colors.green,
-          Icons.person
-      ),
-    ): CircleAvatar(
-      radius: 20,
-      backgroundColor: Colors.white,
-      child: Icon(
-          color: Colors.blue,
-          Icons.person
-      ),
-    ),
-    //TODO: Acabar user list
-    //title: Username_Rating(username: username, rating: rate_user.toString(), edit_button: false),
-    subtitle: Text(last_message_received, style: TextStyle(fontSize: 10, color: Colors.grey)),
   );
 }
 
