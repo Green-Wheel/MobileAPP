99aa0fb4c562c4793ac7bd4006b91eee21a458b8
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2022-12-29 20:34:37 +0100

    Tasca #338: card user refeta, falta enroutament + crida de dades api

--- lib/screens/chat/chat_list_users.dart
+++ lib/screens/chat/chat_list_users.dart
@@ -3,6 +3,8 @@ import 'package:go_router/go_router.dart';
 import 'package:greenwheel/widgets/input_text_message.dart';
 import 'package:greenwheel/widgets/username_rating_stars.dart';
 
+import '../../widgets/card_chat_users.dart';
+
 class ChatListUsers extends StatefulWidget {
   ChatListUsers({Key? key}) : super(key: key);
 
@@ -19,7 +21,48 @@ main(){
   );
 }
 
+class ChatModel {
+  String username;
+  double rate_user;
+  String last_message_received;
+  bool new_message;
+  String last_message_time;
+  ChatModel({required this.username, required this.rate_user, required this.last_message_received, required this.new_message, required this.last_message_time});
+}
+
 class _ChatListUsers extends State<ChatListUsers> {
+
+  List<ChatModel> users = [
+    ChatModel(
+        username: "Michael Jordan",
+        rate_user: 5.0,
+        last_message_received: "see you on the court",
+        new_message: true,
+        last_message_time: "12:00",
+    ),
+    ChatModel(
+        username: "Shaq O'Neil",
+        rate_user: 3.0,
+        last_message_received: "Chicken wings?",
+        new_message: false,
+        last_message_time: "11:00",
+    ),
+    ChatModel(
+        username: "Kobe Bryant",
+        rate_user: 4.5,
+        last_message_received: "Can't stop me fella",
+        new_message: false,
+        last_message_time: "10:00",
+    ),
+    ChatModel(
+        username: "Lebon James",
+        rate_user: 2.5,
+        last_message_received: "I can fly to high in the sky to dunk",
+        new_message: true,
+        last_message_time: "9:00",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,11 +92,20 @@ class _ChatListUsers extends State<ChatListUsers> {
         ),
       ),
       //TODO: Poner listado de usuarios
-      body: SafeArea(
-        child: Stack(
-          children: [],
-        )
-      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 5),
+        child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) => CardChatUsersWidget(
+                username: users[index].username,
+                rate_user: users[index].rate_user,
+                last_message_received: users[index].last_message_received,
+                new_message: users[index].new_message,
+                last_message_time: users[index].last_message_time,
+                context: context,
+                ),
+              )
+      )
     );
   }
 }
--- lib/widgets/card_chat_users.dart
+++ lib/widgets/card_chat_users.dart
@@ -10,120 +10,90 @@ class CardChatUsersWidget extends StatefulWidget {
   double rate_user;
   String last_message_received;
   bool new_message;
-  CardChatUsersWidget({Key? key, required this.username, required this.rate_user, required this.last_message_received, required this.new_message}) : super(key: key);
+  String last_message_time;
+  BuildContext context;
+  CardChatUsersWidget({Key? key, required this.username, required this.rate_user, required this.last_message_received, required this.new_message, required this.last_message_time, required this.context}) : super(key: key);
 
   @override
   State<CardChatUsersWidget> createState() => _CardChatUsersWidget();
 }
 
-main(){
-  bool new_message = true;
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 200),
-          _cardUsers("Miguel Gutiérrez", 3.5, "last message", new_message),
-        ],
-      )
-    ),
-  ),
-  );
-}
 
 class _CardChatUsersWidget extends State<CardChatUsersWidget> {
   @override
   Widget build(BuildContext context) {
-    return _cardUsers(widget.username, widget.rate_user, widget.last_message_received, widget.new_message);
-  }
-}
-
-Widget _cardUsers(String username, double rate_user, String last_message_received, bool new_message) {
-  //TODO: passar context
-  return InkWell(
-    onTap: () {
-      //TODO: implementar ruta de chat
-      new_message = false;
-    },
-    child: SizedBox(
-      height: 80,
-      child: ListTile(
-        shape:  const RoundedRectangleBorder(
-          side: BorderSide(
-            color: Colors.green,
-            width: 1,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        leading: new_message ?  CircleAvatar(
-          radius: 25,
-          backgroundColor: Colors.white,
-          child: Column(
+    return InkWell(
+      onTap: () {
+        //TODO: implementar ruta de chat
+        widget.new_message = false;
+      },
+      child: SizedBox(
+          height: MediaQuery.of(widget.context).size.height * 0.1,
+          width: MediaQuery.of(widget.context).size.width,
+          child: Column (
             children: [
-             Padding(
-               padding: EdgeInsets.only(left: 35),
-               child:  CircleAvatar(
-                 radius: 6,
-                 backgroundColor: Colors.red,
-                 //opcional marcar numero de mensajes no vistos
-                 //child: Text("1", style: TextStyle(color: Colors.white, fontSize: 10),),
-               ),
-             ),
-              Icon(
-                size: 30,
-                  color: Colors.green,
-                  Icons.person
+              ListTile(
+                leading: widget.new_message ?  CircleAvatar(
+                    radius: 27,
+                    backgroundColor: Colors.green[100],
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 12),
+                          child: Icon(
+                              size: 30,
+                              color: Colors.green,
+                              Icons.person
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom:35),
+                          child:  CircleAvatar(
+                            radius: 6,
+                            backgroundColor: Colors.red,
+                            //opcional marcar numero de mensajes no vistos
+                            //child: Text("1", style: TextStyle(color: Colors.white, fontSize: 10),),
+                          ),
+                        ),
+                      ],
+                    )
+                ): CircleAvatar(
+                  radius: 27,
+                  backgroundColor: Colors.green[100],
+                  child: Icon(
+                      size: 30,
+                      color: Colors.green,
+                      Icons.person
+                  ),
+                ),
+                title: Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(widget.context).size.width * 0.52,
+                      child: Column(
+                        children: [
+                          Text(
+                            widget.username,
+                            style: const TextStyle(fontSize: 18, color: Colors.black,),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 28),
+                            child: StarsStaticRateWidget(rate: widget.rate_user),
+                          ),
+                          SizedBox(height: 4),
+                          Text(widget.last_message_received, style: TextStyle(fontSize: 12, color: Colors.grey)),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                trailing: Text(widget.last_message_time, style: const TextStyle(fontSize: 15, color: Colors.black),),
               ),
             ],
           )
-        ): CircleAvatar(
-          radius: 25,
-          backgroundColor: Colors.white,
-          child: Icon(
-              size: 30,
-              color: Colors.green,
-              Icons.person
-          ),
-        ),
-        title: Column(
-          children: [
-            SizedBox(
-              height: 70,
-              child: Stack(
-                children: [
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: SizedBox(
-                        //TODO: retocar width pantalla
-                        width: 250,
-                        child: Column(
-                            children:[
-                              Padding(
-                                padding: EdgeInsets.only(right: 108),
-                                child: Text(username, style: const TextStyle(fontSize: 18, color: Colors.black),),
-
-                              ),
-                              //RatingStars(rating: rate_user.toString()),
-                              StarsStaticRateWidget(rate: rate_user),
-                              //SizedBox(height: 5),
-                              Padding(
-                                padding: EdgeInsets.only(right: 170),
-                                child: Text(last_message_received, style: TextStyle(fontSize: 10, color: Colors.grey)),
-                              ),
-                            ]
-                        ),
-                      )
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Text("12:00", style: const TextStyle(fontSize: 15, color: Colors.black),),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+
