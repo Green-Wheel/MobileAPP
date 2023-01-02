f6577f9f0bbe1edf3d3f0fe1ee9d61e7500feca0
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-01-02 18:48:04 +0100

    Tasca #338: afegit tab entre usuaris i marcadors

--- lib/screens/chat/chat_list_users.dart
+++ lib/screens/chat/chat_list_users.dart
@@ -32,6 +32,37 @@ class ChatModel {
 
 class _ChatListUsers extends State<ChatListUsers> {
 
+  List<ChatModel> markers = [
+    ChatModel(
+      username: "Michael Jackson",
+      rate_user: 5.0,
+      last_message_received: "WOW",
+      new_message: true,
+      last_message_time: "12:00",
+    ),
+    ChatModel(
+      username: "Snoop Dogg",
+      rate_user: 3.0,
+      last_message_received: "After diner... dubadoo",
+      new_message: false,
+      last_message_time: "11:00",
+    ),
+    ChatModel(
+      username: "Bob Marley",
+      rate_user: 4.5,
+      last_message_received: "Happy world weed day",
+      new_message: false,
+      last_message_time: "10:00",
+    ),
+    ChatModel(
+      username: "Steve Colins",
+      rate_user: 2.5,
+      last_message_received: "Sultans of Swing",
+      new_message: true,
+      last_message_time: "9:00",
+    ),
+  ];
+
   List<ChatModel> users = [
     ChatModel(
         username: "Michael Jordan",
@@ -119,66 +150,76 @@ class _ChatListUsers extends State<ChatListUsers> {
     ),
   ];
 
+  Widget ListPoints(BuildContext context, List<ChatModel> list) {
+    return Column(
+      children: [
+        Expanded(
+            child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (context, index) => CardChatUsersWidget(
+                    username: list[index].username,
+                    rate_user: list[index].rate_user,
+                    last_message_received: list[index].last_message_received,
+                    new_message: list[index].new_message,
+                    last_message_time: list[index].last_message_time,
+                    context: context,
+                  ),
+                )
+            )
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Row(
-          children: [
-            IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  //TODO: implementar ruta de chat
-                  GoRouter.of(context).go('/');
-                }
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Row(
+              children: [
+                IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      //TODO: implementar ruta de chat
+                      GoRouter.of(context).go('/');
+                    }
+                ),
+                SizedBox(width: MediaQuery.of(context).size.width * 0.2),
+                const CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                      color: Colors.green,
+                      Icons.chat
+                  ),
+                ),
+                const SizedBox(width: 15),
+                const Text("Chat", style: TextStyle(fontSize: 20, color: Colors.white),)
+              ],
             ),
-            const SizedBox(width: 70),
-            const CircleAvatar(
-              radius: 20,
-              backgroundColor: Colors.white,
-              child: Icon(
-                  color: Colors.green,
-                  Icons.chat
-              ),
+            bottom: const TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(icon: Icon(Icons.location_on_outlined), text: "My Markers", ),
+                Tab(icon: Icon(Icons.supervised_user_circle), text: "Users"),
+              ],
             ),
-            const SizedBox(width: 15),
-            const Text("Chat", style: TextStyle(fontSize: 20, color: Colors.white),)
-          ],
+          ),
+          //TODO: Poner listado de usuarios
+          body: TabBarView(
+            children: [
+              ListPoints(context, markers),
+              ListPoints(context, users),
+            ],
+          ),
         ),
-      ),
-      //TODO: Poner listado de usuarios
-      body: Column(
-        children: [
-          Expanded(
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: ListView.builder(
-                    itemCount: users.length,
-                    itemBuilder: (context, index) => CardChatUsersWidget(
-                      username: users[index].username,
-                      rate_user: users[index].rate_user,
-                      last_message_received: users[index].last_message_received,
-                      new_message: users[index].new_message,
-                      last_message_time: users[index].last_message_time,
-                      context: context,
-                    ),
-                  )
-            )
-          )
-        ],
       ),
     );
   }
 }
-
-/*
-                    FloatingActionButton(
-                        onPressed: () {},
-                        backgroundColor: Colors.green,
-                        child: Icon(
-                            Icons.chat,
-                            color: Colors.white,
-                        ),
-                    )
- */
