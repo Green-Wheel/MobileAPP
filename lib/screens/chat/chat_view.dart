0d3871f04f9281e2dbad8a038bee088554baa7d5
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-01-03 21:08:38 +0100

    Tasca #338: Realitzant infinite list invertida

--- lib/screens/chat/chat_view.dart
+++ lib/screens/chat/chat_view.dart
@@ -11,7 +11,6 @@ import '../../widgets/message_widget.dart';
 class ChatView extends StatefulWidget {
   String username;
   int? id_chat;
-  List<String> messages = [];
   ChatView({Key? key, required this.username, required this.id_chat}) : super(key: key);
 
   @override
@@ -30,6 +29,7 @@ main(){
 class _ChatView extends State<ChatView> {
   TextEditingController _controller = TextEditingController();
   ScrollController _scrollController = ScrollController();
+
   //TODO: servidor de socket
   IO.Socket socket = IO.io('http://localhost:3000', <String, dynamic>{
     'transports': ['websocket'],
@@ -84,10 +84,102 @@ class _ChatView extends State<ChatView> {
     );
   }
 
+  late bool _error;
+  late bool _loading;
+  late int _pageNumber;
+  final int _numberOfPostsPerRequest = 30;
+  final int _nextPageTrigger = 3;
+  List<String> _messages = [];
+
   @override
   void initState() {
     super.initState();
+    _messages = [];
+    _error = false;
+    _loading = true;
+    _pageNumber = _messages.length - 1;
     connect();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      setState(() {
+       // _getBikesList(_pageNumber);
+        //_isFirstPage = _markersListAll.length < _numberOfPostsPerRequest;
+        _loading = false;
+        //_pageNumber = _pageNumber - 1;
+        //_markersListAll.addAll(_markersList);
+      });
+    } catch (e) {
+      print("error --> $e");
+      setState(() {
+        _loading = false;
+        _error = true;
+      });
+    }
+  }
+
+  Widget errorDialog({required double size}){
+    return SizedBox(
+      height: 180,
+      width: 200,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('An error occurred when fetching the posts.',
+            style: TextStyle(
+                fontSize: size,
+                fontWeight: FontWeight.w500,
+                color: Colors.black
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextButton(
+              onPressed:  ()  {
+                setState(() {
+                  _loading = true;
+                  _error = false;
+                  fetchData();
+                });
+              },
+              child: const Text("Retry", style: TextStyle(fontSize: 20, color: Colors.blueAccent),)),
+        ],
+      ),
+    );
+  }
+
+  Widget buildMessagesView() {
+    if (_messages.isEmpty){
+      if(_loading){
+        return const Center(
+            child:Padding(
+              padding: EdgeInsets.all(25),
+              child: CircularProgressIndicator(),
+            ));
+      } else if(_error) {
+        return errorDialog(size: 20);
+      }
+    }
+    return Column(
+      children: [
+        Expanded(
+          //TODO: change a listview.builder
+          child: ListView(
+              controller: _scrollController,
+              children:[
+                //TODO: poner el chat (nuevo fichero)
+                SizedBox(height: 10),
+                MessageWidget(message: "Hola", itsmine: true, read: true, datesend: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now()), dateread:DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now())),
+                MessageWidget(message: "Hola", itsmine: false, read: true, datesend: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now()), dateread: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now())),
+                MessageWidget(message: "Adeu", itsmine: true, read: false, datesend: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now()), dateread: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now())),
+                MessageWidget(message: "HUguvhkgkkcgckgkctgcjcfgcfcfxfcjfjcgcghhhhhhhhhhcghcghcghcghcghtytyt", itsmine: true, read: false, datesend: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now()), dateread: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now())),
+                MessageWidget(message: "HUguvhkgkkcgckgkctgcjcfgcfcfxfcjfjcgcghhhhhhhhhhcghcghcghcghcghtytyt", itsmine: false, read: false, datesend: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now()), dateread: DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now())),
+              ]
+          ),
+        ),
+      ],
+    );
   }
 
   @override
@@ -160,4 +252,5 @@ class _ChatView extends State<ChatView> {
     );
   }
 }
+
 
