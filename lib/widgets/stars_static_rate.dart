aff03364c0649e3c90c6cac41fd71417636cb39b
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-01-13 13:41:39 +0100

    Tasca #338: rating comprimit

--- lib/widgets/stars_static_rate.dart
+++ lib/widgets/stars_static_rate.dart
@@ -49,7 +49,7 @@ Widget _starsStaticCard(double? rate){
         rate != 0.0 ? Padding(
           padding: EdgeInsets.only(left: 5.0),
           child:Text(
-            rate.toString(),
+            rate!.toStringAsFixed(3),
             style: const TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(69, 69, 69, 1)),
           ),
         ): Container(),
